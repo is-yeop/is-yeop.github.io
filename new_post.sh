454cc54b6def85cf5bdca555910841439ef62b86
is-yeop <yeop@yeop.kr>
2021-02-04 14:16:20 +0900

    Add New fuction in Blog
    새로운 기능 두둥등장!
    ./new_post.sh "포스팅 제목" "카테고리" "테크"
    형식으로 사능하다
    만약 태그가 입력되지 않았다면 자동적으로 카테고리와 같은 내용의 태그가 달리게 된다.

---
+++ new_post.sh
@@ -0,0 +1,41 @@
+# get post informations
+TITLE=$1
+CATEGORY=$2
+TAG=$3
+if [[ $TAG == "" ]]; then
+    TAG=$2
+fi
+
+
+# get time
+now=$(date "+%Y-%m-%d")
+
+# make file names
+# bash에서는 띄어쓰기도 중요하다
+file_name="./_posts/${now}-${TITLE}.md"
+
+# echo "${file_name}"
+
+# 이미지 디렉토리 생성
+# 여기도 마찬가지
+mkdir -p "./images/${now}/${TITLE}"
+
+# 파일 생성
+cat <<EOF > ${file_name}
+---
+title: "${TITLE}"
+image:
+    path: /images/${now}/${TITLE}/ 
+categories:
+    - ${CATEGORY}
+tags:
+    - ${TAG}
+last_modified: $(date "+%Y-%m-%dT%H:%M:%S")
+comment: true
+---
+EOF
+
+# 열기
+# 띄어쓰기 입력시 여러 파일 여는 것으로 인식
+# 따옴표로 묶어줘야만함
+code "${file_name}"
